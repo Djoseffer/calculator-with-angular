996d2e2ace6b973f4bbe9ce6449167a3024e3ab3
Djoseffer Palhano Domingues <djoseffer.domingues@compasso.com.br>
2024-09-11 13:21:23 -0300

    program calculator finished

--- src/app/calculator/components/calculator.component.spec.ts
+++ src/app/calculator/components/calculator.component.spec.ts
@@ -1,7 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { CalculatorComponent } from './calculator.component';
-import { CalculatorService} from "../services";
+import {CalculatorComponent} from './calculator.component';
+import {CalculatorService} from "../services";
+import {By} from "@angular/platform-browser";
 
 describe('CalculatorComponent', () => {
   let component: CalculatorComponent;
@@ -9,11 +10,11 @@ describe('CalculatorComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CalculatorComponent ],
-      providers: [ CalculatorService
+      declarations: [CalculatorComponent],
+      providers: [CalculatorService
       ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -24,5 +25,27 @@ describe('CalculatorComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should ensure that 3 + 2 equals 5', () => {
+    let btn3 = fixture.debugElement.query(By.css('#btn3'));
+    let btnSoma = fixture.debugElement.query(By.css('#btnSoma'));
+    let btn2 = fixture.debugElement.query(By.css('#btn2'));
+    let btnCalculate = fixture.debugElement.query(By.css('#btnCalcular'));
+    let display = fixture.debugElement.query(By.css('#display'));
+
+    btn3.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btnSoma.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btn2.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btnCalculate.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(display.nativeElement.value).toEqual(('5'))
   });
 });
