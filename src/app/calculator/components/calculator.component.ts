cc74648c0d793e61f4c1aa80e37c98959038a342
Djoseffer Palhano Domingues <djoseffer.domingues@compasso.com.br>
2024-09-10 22:42:05 -0300

    new features

--- src/app/calculator/components/calculator.component.ts
+++ src/app/calculator/components/calculator.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { CalculatorService} from "../services";
+import {Component, OnInit} from '@angular/core';
+import {CalculatorService} from "../services";
 
 @Component({
   selector: 'app-calculator',
@@ -8,9 +8,88 @@ import { CalculatorService} from "../services";
 })
 export class CalculatorComponent implements OnInit {
 
-  constructor(private calculatorService: CalculatorService) { }
+  private number1: string | null;
+  private number2: string | null;
+  private result: number | null;
+  private operation: string | null;
 
-  ngOnInit(): void {
+  constructor(private calculatorService: CalculatorService) {
+    this.number1 = '0';
+    this.number2 = null;
+    this.result = null;
+    this.operation = null;
   }
 
+  ngOnInit(): void {
+    this.clean();
+  }
+
+  clean(): void {
+    this.number1 = '0';
+    this.number2 = null;
+    this.result = null;
+    this.operation = null;
+  }
+
+  addNumber(numberAdd: string): void {
+    if (this.operation === null) {
+      this.number1 = this.concatNumber(this.number1 ?? '0', numberAdd);
+    } else {
+      this.number2 = this.concatNumber(this.number2 ?? '0', numberAdd);
+    }
+  }
+
+  concatNumber(numberActual: string, numConcat: string): string {
+    if (numberActual === '0' || numberActual === null) {
+      numberActual = '';
+    }
+
+    if (numConcat === '.' && numberActual === '') {
+      return '0.';
+    }
+
+    if (numConcat === '.' && numberActual.indexOf('.') > -1) {
+      return numberActual;
+    }
+
+    return numberActual + numConcat;
+  }
+
+  setOperation(operation: string): void {
+    if (this.operation === null) {
+      this.operation = operation;
+      return;
+    }
+    if (this.number2 !== null) {
+      this.result = this.calculatorService.calculate(
+        parseFloat(this.number1  || '0'),
+        parseFloat(this.number2),
+        this.operation);
+      this.operation = operation;
+      this.number1 = this.result.toString();
+      this.number2 = null;
+      this.result = null;
+    }
+  }
+
+  calculate(): void {
+    if (this.number2 === null) {
+      return;
+    }
+
+    this.result = this.calculatorService.calculate(
+      parseFloat(this.number1  || '0'),
+      parseFloat(this.number2),
+      this.operation ?? '');
+  }
+
+  get display(): string {
+    if (this.result !== null) {
+      return this.result.toString();
+    }
+    if (this.number2 !== null) {
+      return this.number2;
+    }
+    return this.number1  || '0';
+  }
 }
